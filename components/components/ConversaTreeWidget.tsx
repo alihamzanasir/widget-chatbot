de925c89d783a7f029702fa708ff932d6c5b367d
Ali Hamza <155153743+alihamzanasir@users.noreply.github.com>
2026-01-23 01:45:24 +0500

    ,,,

--- components/components/ConversaTreeWidget.tsx
+++ components/components/ConversaTreeWidget.tsx
@@ -5,18 +5,18 @@ import Script from "next/script";
 export default function ConversaTreeWidget() {   
   return (
     <Script
-      src="https://conversa-tree-8ligyvay5-saifgit903s-projects.vercel.app/widget.js"
+      src="https://conversa-tree.vercel.app/widget.js"
       strategy="afterInteractive"
       onLoad={() => {
         // @ts-ignore
         if (window.ConversaTree) {
           // @ts-ignore
           window.ConversaTree.init({
-            apiUrl: "/api/proxy",
-            treeId: 3,
-              domain: "techstore.com",
-            userId:"13"
-          });
+    apiUrl: 'https://conversatreeapi.geniusai.biz/api',
+    treeId: 70,
+            domain: 'techstore.com',
+    userId: 'trueuser'
+  });
         }
       }}
     />
